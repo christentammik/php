9ac9aee97f5f96ee2a1873e7cff59199091337b6
christentammik <156500312+christentammik@users.noreply.github.com>
2024-02-11 16:11:33 +0200

    dadsa
    dsa

---
+++ PHP + BOOTSTRAP kodutöö/teenused.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap harjutus</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container">
+    <div class="top-bar">
+        <div class="row justify-content-end">
+        <div class="col-sm-12 col-md-4">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
+              </svg>
+            your@gmail.com
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+                <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+              </svg>
+            Phone: +5555555
+            | 
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
+              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334q.002-.211-.006-.422A6.7 6.7 0 0 0 16 3.542a6.7 6.7 0 0 1-1.889.518 3.3 3.3 0 0 0 1.447-1.817 6.5 6.5 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.32 9.32 0 0 1-6.767-3.429 3.29 3.29 0 0 0 1.018 4.382A3.3 3.3 0 0 1 .64 6.575v.045a3.29 3.29 0 0 0 2.632 3.218 3.2 3.2 0 0 1-.865.115 3 3 0 0 1-.614-.057 3.28 3.28 0 0 0 3.067 2.277A6.6 6.6 0 0 1 .78 13.58a6 6 0 0 1-.78-.045A9.34 9.34 0 0 0 5.026 15"/>
+              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+              </svg>
+    </div>
+    </div>
+    </div>
+    <div class="row my-4">
+      <div class="col-12 col-md-8 justify-content-end">
+        <div class="logo">
+          <h2>Christen Tammik</h2>
+        </div>
+    </div>
+      <div class="col-6 col-sm-4">
+      <div class="d-flex">
+        <nav class="navbar navbar-expand-lg">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item"><a class="nav-link" href="avaleht.html">Avaleht</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Tooted</a></li>
+            <li class="nav-item"><a class="nav-link" href="teenused.php">Teenused</a></li>
+            <li><button type="button" class="btn btn-primary">Kontakt</button></li>
+          </ul>
+          </div>
+        </nav>
+        </div>
+      </div>
+      </div>
+<div class="calculator">
+  <form action="" method="GET">
+    <div class="row my-4">
+      <div class="col-md-6 offset-md-3">
+        <h3>Teenuse kalkulaator</h3>
+        <div class="mb-3"> 
+          <label for="toode1" class="form-label">Hamburgerid</label>
+          <input type="number" class="form-control" id="toode1" name="toode1" max="200">
+        </div>
+        <div class="mb-3"> <!-- Class mis on valitud bootstrapist -->
+          <label for="toode2" class="form-label">Hot-dogid</label> <!-- toode2 tähistus -->
+          <input type="number" class="form-control" id="toode2" name="toode2"> 
+        </div>
+        <div class="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="noustun" name="noustun">
+          <label class="form-check-label" for="noustun">Nõustun teenusetingimustega</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Arvuta</button>
+      </div>
+    </div>
+  </form>
+</div>
+<?php
+    if (!empty($_GET["toode1"]) && $t2 = !empty($_GET["toode2"])) {
+    //GETiga saatsin, GETiga võtan
+    $t1 = $_GET["toode1"];
+    //Toode 1
+    $t2 = $_GET["toode2"];
+    //Toode 2
+
+    $kokku = $t1+$t2;
+    //Liitmine
+
+    if ($t1 < 0 || $t2 < 0) { //Kui on väiksem kui 0 siis tuleb kirje
+      echo "<div class='row my-4'>"; //Vormistus
+      echo "<div class='col-md-6 offset-md-3'>"; //Vormistus
+      echo "<div class='alert alert-danger' role='alert'>"; //Kirje kui on valesti
+      echo "Sisestatud väärtus ei saa olla negatiivne!";
+      echo "</div>";
+      echo "</div>";
+      echo "</div>";
+  } elseif (!empty($_GET["toode1"]) && !empty($_GET["toode2"]) && empty($_GET["noustun"])) { // Esimesed 2 näitavad, et poleks tühi ja viimane kontrollib veel boxi
+    echo "<div class='row my-4'>";
+    echo "<div class='col-md-6 offset-md-3'>";
+    echo "<div class='alert alert-danger' role='alert'>";
+    echo "Peate nõustuma teenusetingimustega!";
+    echo "</div>";
+    echo "</div>";
+    echo "</div>";
+} else {
+  echo "<div class='row my-4'>";
+  echo "<div class='col-md-6 offset-md-3'>";
+  echo "Hamburgerid: $t1 tk<br>";
+  echo "Hot-dogid: $t2 tk<br>";
+  echo "Kõik kokku: $kokku tk";
+  echo "</div>";
+  echo "</div>";
+}
+    }
+?>
+</div>
+  </body>
+</html>
