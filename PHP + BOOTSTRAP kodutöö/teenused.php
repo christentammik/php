e19eef8ecfbf6cbc99573674a2dd16c587ad4d12
christentammik <156500312+christentammik@users.noreply.github.com>
2024-02-12 22:17:19 +0200

    fdasf
    sdafasdf

--- PHP + BOOTSTRAP kodutöö/teenused.php
+++ PHP + BOOTSTRAP kodutöö/teenused.php
@@ -38,23 +38,23 @@
           <h2>Christen Tammik</h2>
         </div>
     </div>
-      <div class="col-6 col-sm-4">
+    <div class="col-6 col-sm-4">
       <div class="d-flex">
-        <nav class="navbar navbar-expand-lg">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+          <nav class="navbar navbar-expand-lg">
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
               </button>
               <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item"><a class="nav-link" href="avaleht.html">Avaleht</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">Tooted</a></li>
-            <li class="nav-item"><a class="nav-link" href="teenused.php">Teenused</a></li>
-            <li><button type="button" class="btn btn-primary">Kontakt</button></li>
-          </ul>
-          </div>
-        </nav>
-        </div>
+                  <ul class="navbar-nav ms-auto">
+                      <li class="nav-item"><a class="nav-link" href="avaleht.html">Avaleht</a></li>
+                      <li class="nav-item"><a class="nav-link" href="#">Tooted</a></li>
+                      <li class="nav-item"><a class="nav-link" href="teenused.php">Teenused</a></li>
+                      <li class="nav-item"><button type="button" class="btn btn-primary">Kontakt</button></li>
+                  </ul>
+              </div>
+          </nav>
       </div>
+  </div>
       </div>
 <div class="calculator">
   <form action="" method="GET">
